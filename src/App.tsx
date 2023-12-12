feaad04956e5ac91d940ee1915ab2b4e49020e01
sachie5 <sachamccormack@gmail.com>
2023-12-12 17:06:21 +0000

    added search box that filters my name

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import "./App.scss";
+import { FormEvent, useState } from "react";
 import staff from "./data/staff";
 import StaffTile from "./components/StaffTile/StaffTile";
+import SearchStaff from "./components/SearchStaff/SearchStaff";
 
 const App = () => {
+
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  const handleInput = (event: FormEvent<HTMLInputElement>) => {
+    const searchWord = event.currentTarget.value;
+    setSearchTerm(searchWord)
+  }
+
+  const filteredStaff= staff.filter(staff=> {
+    return staff.name.toLowerCase().includes(searchTerm)
+  });
+
   return (
      <div className="tracker">
       <h1 className="tracker__heading">Ticket Tracker</h1>
-        <StaffTile staff={staff}/>
+        <SearchStaff label="Search Staff" searchTerm={searchTerm} handleInput={handleInput}/>
+        <StaffTile staff={filteredStaff}/>
    </div>
  )
 }
---
+++ src/components/SearchStaff/SearchStaff.tsx
@@ -0,0 +1,21 @@
+import { FormEventHandler } from "react";
+import "./SearchStaff.scss";
+
+type SearchStaffProps = {
+    label: string;
+    searchTerm: string;
+    handleInput:FormEventHandler<HTMLInputElement>;
+};
+
+const SearchStaff = ({label, searchTerm, handleInput}: SearchStaffProps) => {
+
+    return (
+        <div className="tracker__search">
+            <label htmlFor={label} className="tracker__search--label">
+                Search: <input type="text" id={label} name={label} value={searchTerm} onInput={handleInput} className="tracker__search--input"/>
+            </label>
+        </div>
+    )
+}
+
+export default SearchStaff;
